27ddadca74130b36fcc157ddb2f73ec44f604538
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-14 08:58:11 +0300

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ element.remove()
 
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
-newHeader.innerHTML = "YOUR-NAME is the champion"
+newHeader.innerHTML = "Brian Koskei is the champion"
